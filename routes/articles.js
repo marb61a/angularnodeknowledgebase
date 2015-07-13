3c77ae98d7e1ba8c41674ff2212e50745e7c7062
marb <marb61a@gmail.com>
2015-07-13 08:31:20 +0000

    updated article routes

--- routes/articles.js
+++ routes/articles.js
@@ -31,4 +31,65 @@ router.get('/category/:category', function(req, res, next) {
 });
 
 
+router.post('/', function(req, res, next){
+  // Get the form values
+  var title = req.body.title;
+	var category = req.body.category;
+	var body = req.body.body;
+	
+	// Constructs article object
+	var newArticle = new Article({
+		title: title,
+		category: category,
+		body: body
+	});
+	
+	// Call the create article method
+	Article.createArticle(newArticle, function(err, article){
+	  if(err){
+			console.log(err);
+		}
+
+		res.location('/articles');
+		res.redirect('/articles');
+	});
+	
+});
+
+
+// Update an article
+router.put('/', function(req, res, next){
+  var id	= req.body.id;
+	var data = {
+		title: req.body.title,
+		category: req.body.category,
+		body: req.body.body
+	};
+	
+	// Create an article
+	Article.updateArticle(id, data, function(err, article){
+	  if(err){
+ 			console.log(err);
+ 		}
+ 		
+		res.location('/articles');
+		res.redirect('/articles');
+	});
+});
+
+// Remove an article
+router.delete('/:id', function(req, res, next){
+  var id	= req.params.id;
+  
+  Article.removeArticle(id, function(err, article){
+		if(err){
+ 			console.log(err);
+ 		}
+		
+		res.location('/articles');
+		res.redirect('/articles');
+	});
+	
+});
+
 module.exports = router;
