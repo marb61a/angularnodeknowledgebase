464dca75bd9c70834ca487503d1d13a1c8e04790
marb <marb61a@gmail.com>
2015-07-15 08:20:31 +0000

    updated files

--- routes/categories.js
+++ routes/categories.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Category = require('../models/category');
 
-//GET users listing. 
+
 router.get('/', function(req, res, next) {
   Category.getCategories(function(err, categories){
   	if(err){
