19275f0a77c3eb567b99be0184848a71896744f3
marb <marb61a@gmail.com>
2015-07-19 08:35:15 +0000

    added create article controller, updated public app.js

--- public/controllers/articles.controller.js
+++ public/controllers/articles.controller.js
@@ -25,3 +25,23 @@ angular.module("kB")
 		    $location.path('/articles');
 	    };
     }])
+    
+    .controller('ArticleCreateCtrl', ['$scope','$http','$routeParams', '$location', function($scope, $http, $routeParams, $location){
+        $http.get('/categories').success(function(data){
+            $scope.categories = data;
+        });
+        
+        $scope.addArticle = function(){
+            var data = {
+                title: $scope.title,
+			    body: $scope.body,
+			    category: $scope.category
+            };
+            
+            $http.post('/articles', data).success(function(data, status){
+                console.log(status);
+            });
+            
+            $location.path('/articles');
+        };
+    }])
