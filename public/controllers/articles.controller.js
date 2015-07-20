cceda2051ad67d5c9d976cb13574feecb2ac921f
marb <marb61a@gmail.com>
2015-07-20 08:12:02 +0000

    added edit controller and updated edit view

--- public/controllers/articles.controller.js
+++ public/controllers/articles.controller.js
@@ -45,3 +45,28 @@ angular.module("kB")
             $location.path('/articles');
         };
     }])
+    
+    .controller('ArticleEditCtrl', ['$scope','$http','$routeParams', '$location', function($scope, $http, $routeParams, $location){
+        $http.get('/categories').success(function(data){
+		    $scope.categories = data;
+	    });
+	    
+	    $http.get('/articles/'+$routeParams.id).success(function(data){
+		    $scope.article = data;
+	    });
+	    
+	    $scope.updateArticle = function(){
+	        var data = {
+    			id: $routeParams.id,
+    			title: $scope.article.title,
+    			body: $scope.article.body,
+    			category: $scope.article.category
+		    };
+
+		$http.put('/articles', data).success(function(data, status){
+			console.log(status);
+		});
+	        
+	        $location.path('/articles');
+	    };
+    }])
